880bd7e01ae419d62512abbdd49dc67f9a4ce11b
tinotendapraise <50191198+tinotendapraise@users.noreply.github.com>
2024-08-04 18:24:57 +0200

    made an api call

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
+function updateWeather(response) {
+  console.log(response.data.temperature.current);
+}
+
+function searchCity(city) {
+  //make api call and update the interface
+  let apiKey = "fafeoa3d37313c099be0t38d47353850";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apikey}`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search");
   let city = document.querySelector("#city");
   city.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchForm = document.querySelector("#search-form");
