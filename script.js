c6c2eed8d9c1feb43867f967db5a9cbf1fbc49b6
tinotendapraise <50191198+tinotendapraise@users.noreply.github.com>
2024-08-04 21:26:42 +0200

    added a link

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 function updateWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
-  let city = document.querySelector("#city");
+  let cityElement = document.querySelector("#city"); // Renamed variable to match element id
 
-  city.innerHTML = response.data.city;
+  cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = Math.round(temperature);
 }
 
 function searchCity(city) {
-  //make api call and update the interface
   let apiKey = "fafeoa3d37313c099be0t38d47353850";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
   axios.get(apiUrl).then(updateWeather);
