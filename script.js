04b1887b87f1b6d2f1d81e5a9f99fed3395817cc
tinotendapraise <50191198+tinotendapraise@users.noreply.github.com>
2024-08-04 20:23:17 +0200

    corrected a typo

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 function updateWeather(response) {
-  console.log(response.data.temperature.current);
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let city = document.querySelector("#city");
+
+  city.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
 }
 
 function searchCity(city) {
   //make api call and update the interface
   let apiKey = "fafeoa3d37313c099be0t38d47353850";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apikey}`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
   axios.get(apiUrl).then(updateWeather);
 }
 
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search");
-  let city = document.querySelector("#city");
-  city.innerHTML = searchInput.value;
+
   searchCity(searchInput.value);
 }
 
